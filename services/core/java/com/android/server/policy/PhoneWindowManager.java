195bcae55a87b3ae592221b37ffa71ce02099193
Roman Birg <roman@cyngn.com>
2016-04-26 16:39:47 -0700

    Only use proximity checks on wakeup keys
    The power button should always wake the screen regardless of the
    proximity; only check proximity when the wake up key code is WAKEUP.
    
    Change-Id: I686ad22d513a3d939be3a0a5c3b752339b2383d3

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6982,7 +6982,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
 
         if (isWakeKey) {
-            wakeUp(event.getEventTime(), mAllowTheaterModeWakeFromKey, "android.policy:KEY", true);
+            wakeUp(event.getEventTime(), mAllowTheaterModeWakeFromKey, "android.policy:KEY",
+                    event.getKeyCode() == KeyEvent.KEYCODE_WAKEUP /* check prox only on wake key*/);
         }
 
         return result;
