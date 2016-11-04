62692ac7d22a862ce1ce5d03b841fec44705ccef
Alex Cruz <mazdarider23@gmail.com>
2016-11-04 06:16:47 +0100

    Optional screenshot type [1/2]
    Change-Id: I0b4dd79566c578063e3764bf0c0ddf1f0687f515
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5306,6 +5306,12 @@ public final class Settings {
         public static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
 
         /**
+         * Whether to use partial screenshot when using volume keys
+         * @hide
+         */
+        public static final String SCREENSHOT_TYPE = "screenshot_type";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
