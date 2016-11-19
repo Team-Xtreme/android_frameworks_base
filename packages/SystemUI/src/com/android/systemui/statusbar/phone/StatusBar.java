aa46ca1a78d355a3b7c8082a4dee650cabec1a83
Calin <nilac8991@gmail.com>
2016-11-19 10:51:25 +0100

    Blur: Implement Recents Blur (1/2)
    - Added basic implementation of serajr Recents blur
    - Adapted the code in order to allow the user to change radius and scale
    - Added required flags in order to avoid Arithmetic exceptions
    
    Change-Id: I13dfed2726a9b34a388217362f33039221c5732b
    Signed-off-by: Calin <nilac8991@gmail.com>
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -525,6 +525,13 @@ public class StatusBar extends SystemUI implements DemoMode,
     private boolean mTranslucentNotifications;
     private int mQSTranslucencyPercentage;
     private int mNotTranslucencyPercentage;
+    private boolean mBlurredRecents;
+    private int mRadiusRecents;
+    private int mScaleRecents;
+
+    private boolean mBlurredRecents;
+    private int mRadiusRecents;
+    private int mScaleRecents;
 
     // RemoteInputView to be activated after unlock
     private View mPendingRemoteInputView;
@@ -1471,14 +1478,14 @@ public class StatusBar extends SystemUI implements DemoMode,
                     if (NotificationPanelView.mKeyguardShowing) {
                         return;
                     }
-                    //RecentsActivity.onConfigurationChanged();
+                    RecentsActivity.onConfigurationChanged();
                     String action = intent.getAction();
 
                if (action.equals(Intent.ACTION_CONFIGURATION_CHANGED)) {
                         if (NotificationPanelView.mKeyguardShowing) {
                             return;
                         }
-                        //RecentsActivity.onConfigurationChanged();
+                        RecentsActivity.onConfigurationChanged();
 
                         if (mExpandedVisible && NotificationPanelView.mBlurredStatusBarExpandedEnabled && (!NotificationPanelView.mKeyguardShowing)) {
                             makeExpandedInvisible();
@@ -6728,11 +6735,31 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.TRANSLUCENT_NOTIFICATIONS_PRECENTAGE_PREFERENCE_KEY),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                 Settings.System.TRANSLUCENT_NOTIFICATIONS_PRECENTAGE_PREFERENCE_KEY),
+                 false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                 Settings.System.RECENT_APPS_ENABLED_PREFERENCE_KEY),
+                 false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                 Settings.System.RECENT_APPS_SCALE_PREFERENCE_KEY),
+                 false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                 Settings.System.RECENT_APPS_RADIUS_PREFERENCE_KEY),
+                 false, this);
         }
 
         @Override
         public void onChange(boolean selfChange, Uri uri) {
-            update();
+                if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.RECENT_APPS_ENABLED_PREFERENCE_KEY))) {
+                    mBlurredRecents = Settings.System.getIntForUser(
+                                        mContext.getContentResolver(),
+                                        Settings.System.RECENT_APPS_ENABLED_PREFERENCE_KEY,
+                                        0, UserHandle.USER_CURRENT) == 1;
+                    RecentsActivity.startBlurTask();
+                    updatePreferences(mContext);
+            } else if { update();
         }
 
         public void update() {
@@ -6893,6 +6920,14 @@ public class StatusBar extends SystemUI implements DemoMode,
                     Settings.System.TRANSLUCENT_QUICK_SETTINGS_PRECENTAGE_PREFERENCE_KEY, 60);
             mNotTranslucencyPercentage = Settings.System.getInt(mContext.getContentResolver(),
                     Settings.System.TRANSLUCENT_NOTIFICATIONS_PRECENTAGE_PREFERENCE_KEY, 70);
+           mBlurredRecents = Settings.System.getIntForUser(resolver,
+                    Settings.System.RECENT_APPS_ENABLED_PREFERENCE_KEY, 0, UserHandle.USER_CURRENT) == 1;
+            mScaleRecents = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.RECENT_APPS_SCALE_PREFERENCE_KEY, 6);
+            mRadiusRecents = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.RECENT_APPS_RADIUS_PREFERENCE_KEY, 3);
+                    
+            RecentsActivity.updateRadiusScale(mScaleRecents,mRadiusRecents);
     }
 
     protected final ContentObserver mNavbarObserver = new ContentObserver(mHandler) {
@@ -8747,7 +8782,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     };
 
     public static void updatePreferences() {
-
+        RecentsActivity.updatePreferences(mContext);
         if (mNotificationData == null)
             return;
 
