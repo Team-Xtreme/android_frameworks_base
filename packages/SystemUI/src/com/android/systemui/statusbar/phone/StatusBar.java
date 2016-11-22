85ee12c3de5eb6f6e4b812a654fdde1c0a5abfcc
Calin <nilac8991@gmail.com>
2016-11-22 23:02:19 +0100

    RecentsBlur: Fix SystemUI crash
    Change-Id: I1c502261d4a8c9bb13e163ef3425be31c8d0b214
    Signed-off-by: Calin <nilac8991@gmail.com>
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1478,7 +1478,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                     if (NotificationPanelView.mKeyguardShowing) {
                         return;
                     }
-                    RecentsActivity.onConfigurationChanged();
                     String action = intent.getAction();
 
                if (action.equals(Intent.ACTION_CONFIGURATION_CHANGED)) {
@@ -1498,7 +1497,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             intent.addAction(Intent.ACTION_CONFIGURATION_CHANGED);
             this.mContext.registerReceiver(receiver, intent);
 
-            //RecentsActivity.init(this.mContext);
+            RecentsActivity.init(this.mContext);
 
             updatePreferences();
         } catch (Exception e){
