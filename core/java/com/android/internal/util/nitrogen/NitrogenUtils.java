5bad5261bcc2e2ac24e8d5446e389cb33e0b930c
maxwen <max.weninger@gmail.com>
2017-08-30 11:24:23 +0200

    [1/2] base: add back qs header images
    AICPfied:
     * added another utility function
     * removed elevation and background attributes to adjust to 8.1
    
    Change-Id: Id9ab770f1c90f609c121b4121c4633baa274149b

--- core/java/com/android/internal/util/nitrogen/NitrogenUtils.java
+++ core/java/com/android/internal/util/nitrogen/NitrogenUtils.java
@@ -64,6 +64,19 @@ public class NitrogenUtils {
         FireActions.toggleCameraFlash();
     }
 
+    public static boolean isPackageAvailable(String packageName, Context context) {
+        Context mContext = context;
+        final PackageManager pm = mContext.getPackageManager();
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            int enabled = pm.getApplicationEnabledSetting(packageName);
+            return enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
+                enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
     public static void sendKeycode(int keycode) {
         long when = SystemClock.uptimeMillis();
         final KeyEvent evDown = new KeyEvent(when, when, KeyEvent.ACTION_DOWN, keycode, 0,
