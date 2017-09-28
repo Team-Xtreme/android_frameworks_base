4ae98d72156c830d5f6f3eafcb30766e28921ece
maxwen <max.weninger@gmail.com>
2017-09-28 19:44:22 +0200

    FWB: all tile scroller for quickbar settings [1/2]
    allow horizontal scrolling through all tiles also
    in the quickbar
    
    AICPfy by @SpiritCroc:
    - Allow fancy animation when scroll position is fine
    - Clip left and right, but not top or bottom
    - Adapt to AICP
    - Bring it to O
    
    PS5:
    - Fix scrollview stealing touches from brightness slider
    
    Change-Id: I2a2090d48ff89ac0f23a27851af842d4aac46da1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4543,6 +4543,12 @@ public final class Settings {
         public static final String HEADSET_CONNECT_PLAYER = "headset_connect_player";
 
         /**
+         * Whether to enable scrolling in the small quick settings
+         * @hide
+         */
+        public static final String QS_QUICKBAR_SCROLL_ENABLED = "qs_quickbar_scroll_enabled";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4708,6 +4714,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(LOCK_TO_APP_ENABLED);
             PRIVATE_SETTINGS.add(EGG_MODE);
             PRIVATE_SETTINGS.add(SHOW_BATTERY_PERCENT);
+            PRIVATE_SETTINGS.add(QS_QUICKBAR_SCROLL_ENABLED);
         }
 
         /**
