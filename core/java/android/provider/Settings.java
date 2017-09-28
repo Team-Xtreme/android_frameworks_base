148debead1d879481bca9c80e7620bea78b3b021
Lars Greiss <kufikugel@googlemail.com>
2017-09-28 14:46:13 +0200

    Option to disable music track title in QS tile [1/2]
    Idea taken from @kufikugel's old 'qs tweeks' commit
    SlimRoms/frameworks_base@4bc584e
    
    Rewritten by @Mazda--
    
    Change-Id: I34d58ad090ff01ddf3d75da52ae06c5973c1f51b
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5017,6 +5017,12 @@ public final class Settings {
                 "status_bar_ticker_animation_mode";
 
         /**
+         * Whether to display music track title within the music qs tile
+         * @hide
+         */
+        public static final String MUSIC_TILE_TITLE = "music_tile_title";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
