bf95f56586a600140d18f9fbacd28e1e50bcf23c
ezio84 <brabus84@gmail.com>
2017-10-06 12:54:22 +0200

    Stock Recents icon pack support [1/2]
    adapted IconPackHelper from CM trebuchet (https://review.lineageos.org/#/c/136600/)
    with some OmniSwitch improvements;
    splitted the whole dialog part and moved it into Settings;
    
    Change-Id: I65cc0360c908801243f37d05212eb864fa08a5e1
    Signed-off-by: Shekhawat2 <anandsingh215@yahoo.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -213,6 +213,7 @@ import com.android.systemui.recents.ScreenPinningRequest;
 import com.android.systemui.recents.events.EventBus;
 import com.android.systemui.recents.events.activity.AppTransitionFinishedEvent;
 import com.android.systemui.recents.events.activity.UndockingTaskEvent;
+import com.android.systemui.recents.misc.IconPackHelper;
 import com.android.systemui.recents.misc.SystemServicesProxy;
 import com.android.systemui.settings.BrightnessController;
 import com.android.systemui.stackdivider.Divider;
@@ -6474,6 +6475,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                   Settings.System.STATUS_BAR_SHOW_TICKER),
                   false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.RECENTS_ICON_PACK),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -6493,6 +6497,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             updateTheme();
             setQsPanelOptions();
             setForceAmbient();
+            updateRecentsIconPack();
         }
     }
 
@@ -6586,6 +6591,13 @@ public class StatusBar extends SystemUI implements DemoMode,
             }
         }
     };
+
+    private void updateRecentsIconPack() {
+        String currentIconPack = Settings.System.getStringForUser(mContext.getContentResolver(),
+            Settings.System.RECENTS_ICON_PACK, mCurrentUserId);
+        IconPackHelper.getInstance(mContext).updatePrefs(currentIconPack);
+        mRecents.resetIconCache();
+    }
 
     private RemoteViews.OnClickHandler mOnClickHandler = new RemoteViews.OnClickHandler() {
 
