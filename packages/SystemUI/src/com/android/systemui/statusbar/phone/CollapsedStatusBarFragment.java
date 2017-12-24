8095a099bbe0256a56c9e7736602eaf00a44073d
ezio84 <brabus84@gmail.com>
2017-12-24 16:36:46 +0100

    Ticker observer code cleanup
    Change-Id: I81d8b51ba79013f8b0bf7858299e2143b1656ef3

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -93,10 +93,6 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         mNetworkController = Dependency.get(NetworkController.class);
         mStatusBarComponent = SysUiServiceProvider.getComponent(getContext(), StatusBar.class);
         mSettingsObserver = new SettingsObserver(new Handler());
-
-        mTickerEnabled = Settings.System.getIntForUser(mContentResolver,
-                Settings.System.STATUS_BAR_SHOW_TICKER, 1,
-                UserHandle.USER_CURRENT);
     }
 
     class SettingsObserver extends UserContentObserver {
@@ -180,7 +176,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         initEmergencyCryptkeeperText();
 
         mSettingsObserver.observe();
-        initTickerView();
+        mSettingsObserver.update();
 
     }
 
