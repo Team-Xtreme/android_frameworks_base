4bb98f5d7270235636afbdd6b89b7dfaf6aebc65
xyyx <xyyx@mail.ru>
2018-01-09 19:36:56 +0100

    Unlock keystore with fingerprint after reboot (1/2)
    Change-Id: I839e6764760cee1a309c47e73ae58a337b033d7a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4969,6 +4969,12 @@ public final class Settings {
                 "statusbar_battery_bar_blend_color_reverse";
 
         /**
+         * Unlock keystore with fingerprint after reboot
+         * @hide
+         */
+        public static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
