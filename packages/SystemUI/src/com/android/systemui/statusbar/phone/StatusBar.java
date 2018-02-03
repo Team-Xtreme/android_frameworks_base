d566b96b2a885ec87a99ed5c99966908610bf801
Pranav Vashi <neobuddy89@gmail.com>
2018-02-03 23:48:12 +0530

    Ticker: Clear ticker regardless of setting
    Issue:
    Ticker enabled on user 0 and disabled on user 1.
    When ticker splashes on user 0 and device switches to user 1,
    tickerview is not cleared - it overlays user 1 lockscreen and hides
    statusbarcomponent of user 1. This is corner case but happened thrice.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3975,7 +3975,6 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         @Override
         public void tickerDone() {
-            if (mTicker == null || mTickerEnabled == 0) return;
             mStatusBarContent.setVisibility(View.VISIBLE);
             mStatusBarContent.startAnimation(loadAnim(false, null));
             if (mTickerView != null) {
@@ -3986,7 +3985,6 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         @Override
         public void tickerHalting() {
-            if (mTicker == null || mTickerEnabled == 0) return;
             if (mStatusBarContent.getVisibility() != View.VISIBLE) {
                 mStatusBarContent.setVisibility(View.VISIBLE);
                 mStatusBarContent
