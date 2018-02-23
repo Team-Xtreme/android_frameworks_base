0ce315acac72465b9e49f1ea4cd9c6a4958c88e0
SpiritCroc <spiritcroc@gmail.com>
2018-02-23 15:51:30 +0100

    Return of the ticker scroll [1/2]
    Bootleggers edit: Set scroll as default because scrolling > fade
    
    Change-Id: Ibedf1541e16e3304d68c2c10eb43a07f3c309c78

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4847,6 +4847,14 @@ public final class Settings {
         public static final String SCREENRECORD_QUALITY_MODE = "screenrecord_quality_mode";
 
         /**
+         * Ticker animation
+         * 0: Fade animation
+         * 1: Scrolling ticker
+         */
+        public static final String STATUS_BAR_TICKER_ANIMATION_MODE =
+                "status_bar_ticker_animation_mode";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
